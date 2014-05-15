5a1fcbdae6abdc000c8e262f072d69b9120f110e
Katherine <khobbs91@gmail.com>
2014-05-15 00:20:50 -0400

    the canvas is behind the chat pane...

--- src/view/CanvasPanel.java
+++ src/view/CanvasPanel.java
@@ -27,27 +27,107 @@ import java.awt.event.*;
 import javax.swing.*;
 
 @SuppressWarnings("serial")
-public class CanvasPanel extends JComponent {
+public class CanvasPanel extends JPanel{
 	private final ChatWindow parent;
     private PrintWriter writer;
     private String convoName;
-	Image image;
-	Graphics2D graphics2D;
-	int currentX, currentY, oldX, oldY;
 
 	public CanvasPanel(String convoName, ChatWindow owner, PrintWriter writer){
 	    this.convoName = convoName;
 	    this.parent = owner;
-	    this.writer = writer; 
-	    setDoubleBuffered(false);
+	    this.writer = writer;
+
+	    Icon iconB = new ImageIcon("blue.gif");
+		Icon iconM = new ImageIcon("magenta.gif");
+		Icon iconR = new ImageIcon("red.gif");
+		Icon iconBl = new ImageIcon("black.gif");
+		Icon iconG = new ImageIcon("green.gif");
+				
+		Container content = this.parent.getContentPane();
+		content.setLayout(new BorderLayout());
+		
+		final PadDraw drawPad = new PadDraw();
+		
+		content.add(drawPad, BorderLayout.CENTER);
+		
+		JPanel panel = new JPanel();
+		panel.setPreferredSize(new Dimension(32, 68));
+		panel.setMinimumSize(new Dimension(32, 68));
+		panel.setMaximumSize(new Dimension(32, 68));
+		
+		JButton clearButton = new JButton("Clear");
+		clearButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				drawPad.clear();
+			}
+		});
+		
+		JButton redButton = new JButton(iconR);
+		redButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				drawPad.red();
+			}
+
+		});
+		
+		JButton blackButton = new JButton(iconBl);
+		blackButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				drawPad.black();
+			}
+		});
+		
+		JButton magentaButton = new JButton(iconM);
+		magentaButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				drawPad.magenta();
+			}
+		});
+		
+		JButton blueButton = new JButton(iconB);
+		blueButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				drawPad.blue();
+			}
+		});
+		
+		JButton greenButton = new JButton(iconG);
+		greenButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				drawPad.green();
+			}
+		});
+		blackButton.setPreferredSize(new Dimension(16, 16));
+		magentaButton.setPreferredSize(new Dimension(16, 16));
+		redButton.setPreferredSize(new Dimension(16, 16));
+		blueButton.setPreferredSize(new Dimension(16, 16));
+		greenButton.setPreferredSize(new Dimension(16,16));
+		
+		panel.add(greenButton);
+		panel.add(blueButton);
+		panel.add(magentaButton);
+		panel.add(blackButton);
+		panel.add(redButton);
+		panel.add(clearButton);
+		
+		content.add(panel, BorderLayout.WEST);
+	}
+}
+
+@SuppressWarnings("serial")
+class PadDraw extends JComponent{
+	Image image;
+	Graphics2D graphics2D;
+	int currentX, currentY, oldX, oldY;
+
+	public PadDraw(){
+		setDoubleBuffered(false);
 		addMouseListener(new MouseAdapter(){
 			public void mousePressed(MouseEvent e){
 				oldX = e.getX();
 				oldY = e.getY();
 			}
 		});
-		//if the mouse is pressed it sets the oldX & oldY
-		//coordinates as the mouses x & y coordinates
 		addMouseMotionListener(new MouseMotionAdapter(){
 			public void mouseDragged(MouseEvent e){
 				currentX = e.getX();
@@ -60,10 +140,47 @@ public class CanvasPanel extends JComponent {
 			}
 
 		});
-		//while the mouse is dragged it sets currentX & currentY as the mouses x and y
-		//then it draws a line at the coordinates
-		//it repaints it and sets oldX and oldY as currentX and currentY
+	}
+
+	public void paintComponent(Graphics g){
+		if(image == null){
+			image = createImage(getSize().width, getSize().height);
+			graphics2D = (Graphics2D)image.getGraphics();
+			graphics2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+			clear();
+
+		}
+		g.drawImage(image, 0, 0, null);
 	}
 
 
+	public void clear(){
+		graphics2D.setPaint(Color.white);
+		graphics2D.fillRect(0, 0, getSize().width, getSize().height);
+		graphics2D.setPaint(Color.black);
+		repaint();
+	}
+	public void red(){
+		graphics2D.setPaint(Color.red);
+		repaint();
+	}
+	public void black(){
+		graphics2D.setPaint(Color.black);
+		repaint();
+	}
+	public void magenta(){
+		graphics2D.setPaint(Color.magenta);
+		repaint();
+	}
+	public void blue(){
+		graphics2D.setPaint(Color.blue);
+		repaint();
+	}
+	public void green(){
+		graphics2D.setPaint(Color.green);
+		repaint();
+	}
+
 }
+
+
